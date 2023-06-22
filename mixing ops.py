de07c02153cc7c488b390f52ab9da950cb3aa129
JonathanCrocker123 <136763157+JonathanCrocker123@users.noreply.github.com>
2023-06-22 18:52:59 -0500

    Add files via upload
    This is showing that you can't use math operators between strings and integers.

---
+++ mixing ops.py
@@ -0,0 +1,7 @@
+#this wont work
+one = 1
+two = 2
+hello = "hello"
+
+print(one + two + hello)
+#you cant mix 2 different operators in one print like this, as it will cause issues printing a int and string
